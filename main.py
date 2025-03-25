2c25509a50065d8be45835c84dcca4b4e73ab447
elmorem <elmorem@gmail.com>
2025-03-25 11:04:06 -0700

    init

---
+++ main.py
@@ -0,0 +1,13 @@
+from dotenv import load_dotenv
+import os
+from langchain_core.messages import HumanMessage, BaseMessage
+from langgraph.graph import END, MessageGraph
+
+load_dotenv(override=True)
+
+
+
+
+
+if __name__ == "__main__":
+    print("hello there")
